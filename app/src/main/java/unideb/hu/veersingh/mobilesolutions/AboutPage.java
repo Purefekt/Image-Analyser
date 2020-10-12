b4263d6f6772d8f2491e368154528d8c19df59d0
Purefekt <roonyyo007@gmail.com>
2020-10-12 20:00:21 +0200

    test

--- app/src/main/java/unideb/hu/veersingh/mobilesolutions/AboutPage.java
+++ app/src/main/java/unideb/hu/veersingh/mobilesolutions/AboutPage.java
@@ -11,7 +11,6 @@ public class AboutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
-
         //Portrait locking
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
